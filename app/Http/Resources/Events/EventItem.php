af64135f7de2379956f29d828277d643c1cb78f3
rafie <rafiekom@gmail.com>
2022-03-06 01:23:35 +0800

    event return

--- app/Http/Resources/Events/EventItem.php
+++ app/Http/Resources/Events/EventItem.php
@@ -15,24 +15,24 @@ class EventItem extends JsonResource
     public function toArray($request)
     {
           return  [
-            'id'      => $this->resource->id,
-            "store_id" => $this->resource->store_id,
+            'id'      => intval($this->resource->id),
+            "store_id" => intval($this->resource->store_id),
             "store_name" => $this->resource->stores->name,
             "name" => $this->resource->name,
             "date" => $this->resource->date,
             "date_end" => $this->resource->date_end,
             "term_condition" => $this->resource->term_condition,
             "description" => $this->resource->description,
-            "price" => $this->resource->price,
-            "point_cashback" => $this->resource->point_cashback,
+            "price" => intval($this->resource->price),
+            "point_cashback" => intval($this->resource->point_cashback),
             "image" => $this->resource->image(),
             "category" => $this->resource->category,
             "online_link" => $this->resource->online_link,
-            "subscription" => 0,
+            "subscription" => intval(0),
             "is_kouta" => $this->resource->kouta == null ? false : true,
-            "kouta" => $this->resource->kouta,
-            "min_purchased" => $this->resource->min_purchased,
-            "max_purchased" => $this->resource->max_purchased,
+            "kouta" => strval($this->resource->kouta),
+            "min_purchased" => intval($this->resource->min_purchased),
+            "max_purchased" => intval($this->resource->max_purchased),
         ];
     }
 }
